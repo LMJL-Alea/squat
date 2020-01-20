fcfeff6c7dcb4ddb478e0aba3f8c77a7783c3c92
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2020-01-20 16:08:19 +0100

    Add simple R function to normalize quaternion time series.

--- R/distances.R
+++ R/distances.R
@@ -16,3 +16,41 @@ gdistance <- function(x, y) {
   if (x > 1) x <- 1
   2 * acos(x)
 }
+
+qinv <- function(x) {
+  x[2:4] <- -x[2:4]
+  x
+}
+
+qprod <- function(x, y) {
+  x1 <- x[1]
+  x2 <- x[2]
+  x3 <- x[3]
+  x4 <- x[4]
+  y1 <- y[1]
+  y2 <- y[2]
+  y3 <- y[3]
+  y4 <- y[4]
+  c(
+    x1 * y1 - x2 * y2 - x3 * y3 - x4 * y4,
+    x1 * y2 + x2 * y1 + x3 * y4 - x4 * y3,
+    x1 * y3 + x3 * y1 - x2 * y4 + x4 * y2,
+    x4 * y1 + x1 * y4 + x2 * y3 - x3 * y2
+  )
+}
+
+#' Quaternion time series normalization
+#'
+#' @param x A 4-row numeric matrix.
+#'
+#' @return A 4-row numeric matrix.
+#'
+#' @export
+#' @examples
+#' qts <- matrix(rnorm(40), 4, 10)
+#' qts <- apply(qts, 2, function(.x) .x / sqrt(sum(.x^2)))
+#' normalize_qts(qts)
+normalize_qts <- function(x) {
+  q_init <- qinv(x[, 1])
+  apply(x, 2, function(.x) qprod(q_init, .x))
+}
