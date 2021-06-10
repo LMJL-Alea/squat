e631fc6897eceb5d226785d08fcb92a46a9eb277
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2021-06-10 17:46:04 +0200

    Added a function to add noise to QTS.

--- R/qts.R
+++ R/qts.R
@@ -18,3 +18,62 @@ derivative_qts <- function(qts) {
   qts <- derivative_qts_impl(qts)
   qts[-1, ]
 }
+
+#' QTS Noise Generator
+#'
+#' This function adds uncorrelated Gaussian noise to the logarithm QTS using an
+#' exponential covariance function.
+#'
+#' See \code{\link[roahd]{exp_cov_function}} for details about the roles of
+#' `alpha` and `beta` in the definition of the covariance operator.
+#'
+#' @param qts A \code{\link[tibble]{tibble}} specifying the input QTS.
+#' @param n An integer specifying how many noisy QTS should be generated.
+#'   Defaults to `1L`.
+#' @param alpha A positive scalar specifying the variance of each component of
+#'   the log-QTS. Defaults to `0.2`.
+#' @param beta A positive scalar specifying the exponential weight. Defaults to
+#'   `0.5`.
+#'
+#' @return A list of `n` QTS with added noise as specified by parameters `alpha`
+#'   and `beta`.
+#' @export
+#'
+#' @examples
+#' qts <- tibble(
+#'   time = seq(0, 1, len = 101),
+#'   w = 0,
+#'   x = cos(2 * pi * time),
+#'   y = time^2,
+#'   z = sin(time)
+#' )
+#' qts <- exp_qts(qts)
+#' qts_n <- add_noise(qts, n = 10)
+add_noise <- function(qts, n = 1, alpha = 0.1, beta = 0.5) {
+  qts <- log_qts(qts)
+  time_grid <- qts$time
+  C1 <- roahd::exp_cov_function(time_grid, alpha = alpha, beta = beta)
+  C2 <- roahd::exp_cov_function(time_grid, alpha = alpha, beta = beta)
+  C3 <- roahd::exp_cov_function(time_grid, alpha = alpha, beta = beta)
+  centerline <- rbind(qts$x, qts$y, qts$z)
+  CholC1 <- chol(C1)
+  CholC2 <- chol(C2)
+  CholC3 <- chol(C3)
+  qts <- roahd::generate_gauss_mfdata(
+    N = n,
+    L = 3,
+    centerline = centerline,
+    correlations = rep(0, 3),
+    listCholCov = list(CholC1, CholC2, CholC3)
+  )
+  qts_list <- lapply(1:n, function(.x) {
+    tibble(
+      time = time_grid,
+      w = 0,
+      x = qts[[1]][.x, ],
+      y = qts[[2]][.x, ],
+      z = qts[[3]][.x, ]
+    )
+  })
+  lapply(qts_list, exp_qts)
+}
