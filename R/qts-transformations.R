ab03be81499b54eb435aba508c5db7e19d781c98
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2022-09-21 07:16:25 +0200

    Fix naming issue in example.

--- R/qts-transformations.R
+++ R/qts-transformations.R
@@ -15,7 +15,7 @@
 #'
 #' @export
 #' @examples
-#' qts2distance(vespa64$igp[[1]], vespa64$igp[[2]])
+#' qts2dts(vespa64$igp[[1]], vespa64$igp[[2]])
 qts2dts <- function(x, y) {
   if (!is_qts(x))
     cli::cli_abort("The input argument {.arg x} should be of class {.cls qts}.")
