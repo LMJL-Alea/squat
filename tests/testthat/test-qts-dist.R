314074b7b3d3cba9888a818f3d796a3257a96d07
Aymeric Stamm <aymeric.stamm@cnrs.fr>
2023-06-01 08:30:30 +0200

    Fix test suite.

--- tests/testthat/test-qts-dist.R
+++ tests/testthat/test-qts-dist.R
@@ -2,15 +2,15 @@ test_that("Function dist.default() works", {
   x <- c(0, 0, 1, 1, 1, 1)
   y <- c(1, 0, 1, 1, 0, 1)
   D <- dist(rbind(x, y), method = "binary")
-  expect_snapshot(D)
+  expect_snapshot(round(D, 5))
 })
 
 test_that("Function dist.qts_sample() works via fdacluster pkg", {
   D <- dist(vespa64$igp[1:5])
-  expect_snapshot(D)
+  expect_snapshot(round(D, 5))
 })
 
 test_that("Function dist.qts_sample() works via dtw pkg", {
   D <- dist(vespa64$igp[1:5], metric = "dtw")
-  expect_snapshot(D)
+  expect_snapshot(round(D, 5))
 })
